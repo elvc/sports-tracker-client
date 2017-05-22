863c74b729150618fcb389b971cdaa0ebccec2c3
Elvis Chan <choectt@gmail.com>
2017-05-21 22:21:33 -0700

    refactor

--- app/components/sidebar/SideBar.jsx
+++ app/components/sidebar/SideBar.jsx
@@ -11,44 +11,44 @@ export default class Sidebar extends Component {
         <div className="brand">Sports</div>
         <i className="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content" />
         <div className="menu-list">
-
-        </div>
-
-      <div className="nav-side-menu">
-        <div className="brand">Sports Scoreboard</div>
-        <i className="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content" />
-        <div className="menu-list">
           <ul id="menu-content" className="menu-content collapse out">
-            <li><a href="#"><i className="fa fa-dashboard fa-lg" /> Dashboard</a></li>
             <li><a href="#"><i className="fa fa-user fa-lg" /> Login</a></li>
             <li><a href="#"><i className="fa fa-users fa-lg" /> Register</a></li>
           </ul>
         </div>
-        
-        <li data-toggle="collapse" data-target="#nhl" className="collapsed">
-          <a href=""><img src="/img/hockey.png" alt="nhl" /> NHL <span className="arrow" /></a>
-        </li>
+
+        <ul>
+          <li data-toggle="collapse" data-target="#nhl" className="collapsed">
+            <a href=""><img src="/img/hockey.png" alt="nhl" /> NHL <span className="arrow" /></a>
+          </li>
+        </ul>
         <ul className="sub-menu collapse" id="nhl">
 
         </ul>
 
-        <li data-toggle="collapse" data-target="#nfl" className="collapsed">
-          <a href=""><img src="/img/sport_football.png" alt="nfl" /> NFL <span className="arrow" /></a>
-        </li>
+        <ul>
+          <li data-toggle="collapse" data-target="#nfl" className="collapsed">
+            <a href=""><img src="/img/sport_football.png" alt="nfl" /> NFL <span className="arrow" /></a>
+          </li>
+        </ul>
         <ul className="sub-menu collapse" id="nfl">
 
         </ul>
 
-        <li data-toggle="collapse" data-target="#mlb" className="collapsed">
-          <a href=""><img src="/img/baseball-icon.png" alt="mlb" /> MLB <span className="arrow" /></a>
-        </li>
+        <ul>
+          <li data-toggle="collapse" data-target="#mlb" className="collapsed">
+            <a href=""><img src="/img/baseball-icon.png" alt="mlb" /> MLB <span className="arrow" /></a>
+          </li>
+        </ul>
         <ul className="sub-menu collapse" id="mlb">
 
         </ul>
 
-        <li data-toggle="collapse" data-target="#nba" className="collapsed">
-          <a href=""><img src="/img/Basketball-48.png" alt="nba" /> NBA <span className="arrow" /></a>
-        </li>
+        <ul>
+          <li data-toggle="collapse" data-target="#nba" className="collapsed">
+            <a href=""><img src="/img/Basketball-48.png" alt="nba" /> NBA <span className="arrow" /></a>
+          </li>
+        </ul>
         <ul className="sub-menu collapse" id="nba">
 
         </ul>
