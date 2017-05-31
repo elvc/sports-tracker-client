7eb664132dc5c87dc9b674b70927605a46aa3fdf
elvc <choectt@gmail.com>
2017-05-31 11:47:35 -0700

    Registration form validations (#81)
    * Smaller font. Tabular structure for displaying messages
    
    * smaller input field
    
    * Added league logos
    
    * Resolve conflicts
    
    * Resolve conflict
    
    * Decreased line height of messages. Defined max-width for message container
    
    * set up tabs for rooms
    
    * WIP
    
    * added flexbox to cards
    
    * resolve conflicts
    
    * registration form created
    
    * addressed CORS
    
    * Registration form modal complete
    
    * WIP
    
    * minor sidebar layout change
    
    * File cleanup
    
    * refactor user login/reg using Redux
    
    * refactor using Redux
    
    * minor fix on sidebar margins on mobile
    
    * prevent background elements to scroll while sidebar is expanded on mobile
    
    * sidebar layout changes. prevent body scrolling with sidebar expanded on mobile
    
    * minor body scrolling changes
    
    * fix scrolling issue on desktop screen
    
    * username and password length validation
    
    * input validation for registration form

--- app/components/modals/RegForm.jsx
+++ app/components/modals/RegForm.jsx
@@ -13,7 +13,10 @@ export default class RegForm extends Component {
     this.state = {
       username: '',
       email: '',
-      password: ''
+      password: '',
+      usernameLengthValid: false,
+      passwordLengthValid: false,
+      emailValid: false
     };
   }
 
@@ -29,6 +32,60 @@ export default class RegForm extends Component {
 
   handleKeyChange = key => (event) => {
     this.setState({ [key]: event.target.value });
+    // validate username, email, and password input
+    if (key === 'username') {
+      // alphanumeric
+      const pass = /^\w{5}/.test(event.target.value);
+
+      if (!pass) {
+        $('.username-input').addClass('has-danger');
+        $('.username-feedback').show();
+        $('.username-warning-placeholder').hide();
+        this.state.usernameLengthValid = false;
+      } else {
+        $('.username-input').removeClass('has-danger');
+        $('.username-feedback').hide();
+        $('.username-warning-placeholder').show();
+        this.state.usernameLengthValid = true;
+      }
+    }
+    if (key === 'email') {
+      const pass = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(event.target.value);
+
+      if(!pass) {
+        $('.email-input').addClass('has-danger');
+        $('.email-feedback').show();
+        $('.email-warning-placeholder').hide();
+        this.state.emailValid = false;
+      } else {
+        $('.email-input').removeClass('has-danger');
+        $('.email-feedback').hide();
+        $('.email-warning-placeholder').show();
+        this.state.emailValid = true;
+      }
+    }
+    if (key === 'password') {
+      const pass = /^\w{8}/.test(event.target.value);
+
+      if (!pass) {
+        $('.password-input').addClass('has-danger');
+        $('.password-feedback').show();
+        $('.password-warning-placeholder').hide();
+        this.state.passwordLengthValid = false;
+      } else {
+        $('.password-input').removeClass('has-danger');
+        $('.password-feedback').hide();
+        $('.password-warning-placeholder').show();
+        this.state.passwordLengthValid = true;
+      }
+    }
+
+    // cannot submit form unless all inputs are valid
+    if (this.state.usernameLengthValid && this.state.emailValid && this.state.passwordLengthValid) {
+      $('#reg-submit').prop('disabled', false);
+    } else {
+      $('#reg-submit').prop('disabled', true);
+    }
   }
 
   handleSubmit = (e) => {
@@ -88,16 +145,16 @@ export default class RegForm extends Component {
     return (
       <div>
         <h3 className="pl-0 d-flex modal-header">
-        Registration: <i className="fa fa-times justify-content-right" onClick={ this.props.close } />
+        Registration: <i className="fa fa-times justify-content-right"onClick={ this.props.close } />
         </h3>
         <form onSubmit={ this.handleSubmit } >
-          <div className="form-group row pr-3 pl-3">
+          <div className="username-input form-group row pr-3 pl-3 mb-0">
             <label
               htmlFor="formUser"
               className="col-form-label-sm"
             >
-            User Name:
-          </label>
+            User Name: (minimum 5 characters)
+            </label>
             <input
               id="formUser"
               className="form-control"
@@ -105,40 +162,51 @@ export default class RegForm extends Component {
               placeholder="Your username"
               type="text"
               onChange={ this.handleKeyChange('username') }
+              required
             />
+            <div className="username-warning-placeholder">&nbsp;</div>
+            <div className="username-feedback form-control-feedback hide">The username requires a minimum of 5 characters</div>
           </div>
-          <div className="form-group row pl-3 pr-3">
+          <div className="email-input form-group row pl-3 pr-3 mb-0">
             <label
               htmlFor="formEmail"
               className="col-form-label-sm"
             >
             Email:
-          </label>
+            </label>
             <input
               id="formEmail"
-              className="form-control"
+              className="form-control form-control-warning"
               name="email"
               placeholder="user@example.com"
               type="email"
               onChange={ this.handleKeyChange('email') }
+              required
+              minLength="5"
             />
+          <div className="email-warning-placeholder">&nbsp;</div>
+            <div className="email-feedback form-control-feedback hide">A valid email address is required</div>
           </div>
-          <div className="form-group row pl-3 pr-3">
+          <div className="password-input form-group row pl-3 pr-3 mb-0">
             <label
               htmlFor="formPassword"
               className="col-form-label-sm"
             >
-            Password:
-          </label>
+            Password: (minimum 8 characters)
+            <span className="password-warning-placeholder" />
+            </label>
             <input
               id="formPassword"
               className="form-control"
               name="password"
               type="password"
               onChange={ this.handleKeyChange('password') }
+              required
             />
+            <div className="password-warning-placeholder">&nbsp;</div>
+            <div className="password-feedback form-control-feedback hide">Password requires a minimum of 8 characters</div>
           </div>
-          <button className="btn btn-primary pull-right" type="submit">Sign up</button>
+          <button id="reg-submit" className="btn btn-primary pull-right" disabled="disabled" type="submit">Sign up</button>
         </form>
       </div>
     );
